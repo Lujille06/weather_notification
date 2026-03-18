5cc711a27d6b8198c6547cbcd31a0869009aa6bc
Lujille06 <crisartuz.c@gmail.com>
2026-03-18 15:33:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ import requests
 from smtplib import SMTP
 import os 
 
-API_KEY = os.environ.get("OWM_API_KEY")
-if API_KEY:
-    print(f"Key loaded: {API_KEY[0]}...{API_KEY[-1]} (Length: {len(API_KEY)})")
-else:
-    print("Key not found in environment!")
-    
+API_KEY = os.environ.get("OWM_API_KEY")   
 MY_LAT = os.environ.get("MY_LAT")
 MY_LONG = os.environ.get("MY_LON")
 
